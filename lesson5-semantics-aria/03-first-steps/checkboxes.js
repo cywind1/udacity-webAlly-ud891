ba2d1a6c1e7335f7ed3b951c5a1ce362a4e7bd13
cywind1 <chingjusttest@gmail.com>
2022-06-28 10:52:23 +0200

    5.3_attribute_ARIA_inSync

--- lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -1,13 +1,13 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
   /** Define values for keycodes */
-  var VK_ENTER      = 13;
-  var VK_SPACE      = 32;
-  var VK_LEFT       = 37;
-  var VK_UP         = 38;
-  var VK_RIGHT      = 39;
-  var VK_DOWN       = 40;
+  var VK_ENTER = 13;
+  var VK_SPACE = 32;
+  var VK_LEFT = 37;
+  var VK_UP = 38;
+  var VK_RIGHT = 39;
+  var VK_DOWN = 40;
 
   /** Helper function to convert NodeLists to Arrays */
   function slice(nodes) {
@@ -17,14 +17,23 @@
   function Checkbox(el) {
     this.el = el;
 
-    this.el.addEventListener('keydown', this.handleKeyDown.bind(this));
-    this.el.addEventListener('click', this.toggle.bind(this));
+    this.el.addEventListener("keydown", this.handleKeyDown.bind(this));
+    this.el.addEventListener("click", this.toggle.bind(this));
 
-    // Any other set-up we want to do here?
+    // Change-1
+    // set the role as checkbox
+    this.el.setAttribute("role", "checkbox");
+    if (this.el.hasAttribute("checked")) {
+      // set the aria-checked as true or false
+      this.el.setAttribute("aria-checked", "true");
+    } else {
+      this.el.setAttribute("aria-checked", "false");
+    }
+    //
   }
 
-  Checkbox.prototype.handleKeyDown = function(e) {
-    switch(e.keyCode) {
+  Checkbox.prototype.handleKeyDown = function (e) {
+    switch (e.keyCode) {
       case VK_ENTER:
       case VK_SPACE: {
         this.toggle();
@@ -33,22 +42,18 @@
     }
   };
 
-  Checkbox.prototype.toggle = function() {
-    if (this.el.hasAttribute('checked')) {
-      this.el.removeAttribute('checked');
-
-      // Hmm.
-
+  Checkbox.prototype.toggle = function () {
+    if (this.el.hasAttribute("checked")) {
+      this.el.removeAttribute("checked");
+      // Change-2
+      // Keep Attribute & aria in sync
+      this.el.setAttribute("aria-checked", "false");
     } else {
-      this.el.setAttribute('checked', '');
-
-      // Hmmmmm.
-
+      this.el.setAttribute("checked", "");
+      this.el.setAttribute("aria-checked", "true");
     }
   };
 
-  var checkboxes = slice(document.querySelectorAll('.checkbox'));
-  for (var checkbox of checkboxes)
-    checkbox.logic = new Checkbox(checkbox);
-
-}());
+  var checkboxes = slice(document.querySelectorAll(".checkbox"));
+  for (var checkbox of checkboxes) checkbox.logic = new Checkbox(checkbox);
+})();
